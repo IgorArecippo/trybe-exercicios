09e973dab3d345374ff14eb1fb6067fec0fcc95f
Igor Arecippo <iarecippo@gmail.com>
2022-04-06 20:16:56 -0300

    exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -1,4 +1,4 @@
-//1
+/*1
 const a = 5;
 const b = 15;
 
@@ -8,10 +8,36 @@ console.log(a * b);
 console.log(a / b);
 console.log(a % b);
 
-//2
+2
 const c = 10;
 const d = 20;
 
 if (c > d) {
     console.log("c maior que d");
-} else console.log("d maior que c")
+} else {
+    console.log("d maior que c");
+} 
+
+
+3
+const e = 30;
+const f = 31;
+const g = 32;
+
+if (e > f && e > g){
+    console.log('e maior número');
+} else if (f > e && f > g){
+    console.log('f maior número');
+} else{
+    console.log('g maior número');
+}
+
+4*/
+const valor = 0;
+if (valor > 0){
+    console.log('positive');
+} else if (valor < 0) {
+    console.log('negative');
+} else {
+    console.log('zero');
+};
