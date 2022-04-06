ca2a75fd66b70cee2b5c7210130550602b7eda56
Igor Arecippo <iarecippo@gmail.com>
2022-04-06 19:10:44 -0300

    Create exercicio.js
    exercicios 1 e 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -0,0 +1,17 @@
+//1
+const a = 5;
+const b = 15;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//2
+const c = 10;
+const d = 20;
+
+if (c > d) {
+    console.log("c maior que d");
+} else console.log("d maior que c")
