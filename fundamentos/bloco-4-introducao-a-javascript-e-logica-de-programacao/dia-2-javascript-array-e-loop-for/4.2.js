44080ca5d0faf448beca988df3795cd03cbe9b5a
Igor Arecippo <iarecippo@gmail.com>
2022-04-08 10:59:26 -0300

    exercicio5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2.js
@@ -2,11 +2,33 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
  /*1:
 for(let i = 0; i < numbers.length; i += 1){
     console.log(numbers[i]);
-}*/
 
-//2
+
+2
 let soma = 0
 for(let i =0; i < numbers.length; i += 1){
     soma += numbers[1];
 }
-console.log(soma)
+console.log(soma)}
+
+3; 4 
+let soma = 0
+for(let i = 0; i < numbers.length; i += 1){
+    soma += numbers[i];
+}
+let media = soma / numbers.length;
+
+if(media > 20){
+    console.log('valor maior que 20');
+} else{
+    console.log('valor menor ou igual a 20');
+};*/
+
+//5
+let maiorNumero = numbers[0];
+for(let i = 0; i < numbers.length; i += 1){
+    if(numbers[i] > maiorNumero){
+        maiorNumero = numbers[i];
+    }
+};
+console.log(maiorNumero)
