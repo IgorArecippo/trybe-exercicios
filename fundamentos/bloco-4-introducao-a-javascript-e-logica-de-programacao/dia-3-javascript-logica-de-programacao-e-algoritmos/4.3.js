9b5996693e283021282118ea1822f353f69fa522
Igor Arecippo <iarecippo@gmail.com>
2022-04-10 12:34:41 -0300

    exercicio4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4.3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4.3.js
@@ -1,5 +1,37 @@
+/*1
 let answer = 10
 for (let i = answer; i >= 1; i -= 1) {
     answer = i * answer;
 };
 console.log(answer);
+
+2
+let word = 'tryber';
+let word2 = '';
+for (i = word.length - 1; i >= 0; i -= 1) {
+    word2 += word[i];
+}
+console.log(word2);
+
+3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let menorPalavra = array[0];
+let maiorPalavra = array[0];
+
+for (let i = 0; i < array.length; i += 1){
+    if (array[i].length > maiorPalavra.length){
+        maiorPalavra = array[i];
+    }
+}
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length < menorPalavra.length) {
+        menorPalavra = array[i];
+    }
+}
+ console.log(maiorPalavra);
+ console.log(menorPalavra);
+
+ 4*/
+ 
