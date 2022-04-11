7253e3cd55b1e97efea4e7cad9523b755058f445
Igor Arecippo <iarecippo@gmail.com>
2022-04-11 20:38:29 -0300

    exercicio2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4.js
@@ -0,0 +1,19 @@
+/*PARTE I
+
+1*
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Bem-vinda, ' + info.personagem);
+
+  2*/
+  let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+  info.recorrente = 'Sim';
+  console.log(info);
