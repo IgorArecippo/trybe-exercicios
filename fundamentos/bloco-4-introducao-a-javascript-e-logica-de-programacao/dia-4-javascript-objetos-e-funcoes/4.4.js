456f514e14a1e02f2dbc7c882ab08fead6bfbaf5
Igor Arecippo <iarecippo@gmail.com>
2022-04-11 21:01:39 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4.js
@@ -1,6 +1,12 @@
-/*PARTE I
+//PARTE I
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
 
-1*
+/*1
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -9,11 +15,37 @@ let info = {
 
   console.log('Bem-vinda, ' + info.personagem);
 
-  2*/
-  let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+  2
   info.recorrente = 'Sim';
   console.log(info);
+
+  3
+  for(let i in info) {
+      console.log(i);
+  };
+
+  4
+  for(let i in info) {
+      console.log(info[i]);
+  };
+
+  5
+  let info2 = {
+      personagem: 'Tio Patinhas',
+      origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+      nota: 'O último MacPatinhas',
+      recorrente: 'Sim'
+  };
+
+  for(let i in info) {
+      if (
+          i === 'recorrente' && info[i] === 'Sim' && info2[i] === 'Sim'
+      ) {
+          console.log('Ambos recorrentes');
+      } else {
+          console.log(info[i] + ' e ' + info2[i]);
+      }
+  }
+
+  6*/
+  
