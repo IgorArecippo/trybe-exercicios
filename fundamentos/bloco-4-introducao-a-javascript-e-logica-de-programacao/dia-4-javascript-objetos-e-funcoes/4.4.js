40cb562ae072f0bf4045515bca7969cf200328a4
Igor Arecippo <iarecippo@gmail.com>
2022-04-11 21:21:10 -0300

    parte 1 completa

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4.js
@@ -1,4 +1,4 @@
-//PARTE I
+/*PARTE I
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
@@ -6,7 +6,7 @@ let info = {
   recorrente: 'Sim'
 };
 
-/*1
+1
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -47,5 +47,34 @@ let info = {
       }
   }
 
-  6*/
-  
+  6
+  let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + 'se chama ' + leitor['livrosFavoritos'][0]['titulo']);
+
+7
+leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+    }
+)
+
+8
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+
+PARTE II
+
+1*/
