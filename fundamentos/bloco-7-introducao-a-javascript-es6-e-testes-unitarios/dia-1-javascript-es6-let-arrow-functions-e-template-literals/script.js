2313c077f5bf09c5126686cb4a8950e71fa40a94
Igor Arecippo <iarecippo@gmail.com>
2022-07-14 19:23:27 -0300

    parte 2 exercicio 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-arrow-functions-e-template-literals/script.js
@@ -34,3 +34,18 @@ console.log(factorial(7))
 //forma recursiva:
 const fatorial = (number) => number > 1 ? number * factorial(number - 1) : 1;
 console.log(fatorial(3))
+
+//2
+const longestWord = (text) => {
+    const wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
+
+    for (const word of wordArray){
+        if (word.length > maxLength){
+            result = word
+        }
+    }
+    return result;
+}
+console.log(longestWord('o canalha tem a senha que te deixa fraquinha o canalha tem a senha q te deixa fraquinha'));
