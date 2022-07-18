f490b8da70f9e73cdbe696b5fc6c081db583e2fb
Igor Arecippo <iarecippo@gmail.com>
2022-07-18 13:04:50 -0300

    parte 3 exercicios 2-7

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script2.js
@@ -27,4 +27,34 @@ addNigth(lesson2, 'turno', 'noite');
 console.log(lesson2);
 
 //2
+const listKey = (obj) => Object.keys(obj);
+console.log(listKey(lesson1));
 
+//3
+const keysLength = (obj) => Object.keys(obj).length;
+console.log(keysLength(lesson2));
+
+//4
+const listValues = (obj) => Object.values(obj);
+console.log(listValues(lesson3));
+
+//5
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+
+//6
+const numberStudents = (obj) => {
+    let total = 0;
+    const array = Object.keys(obj);
+    for (index in array) {
+        total += obj[array[index]].numeroEstudantes
+    }
+    return total;
+}
+console.log(numberStudents(allLessons))
+
+//7
+const getValue = (obj, number) => Object.values(obj)[number];
+console.log(getValue(lesson3, 2));
+
+//8
