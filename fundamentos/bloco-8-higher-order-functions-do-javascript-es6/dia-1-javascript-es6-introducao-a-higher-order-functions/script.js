7658c3e3aaa05f85a009967bad3ad61db602d971
Igor Arecippo <iarecippo@gmail.com>
2022-07-21 19:44:08 -0300

    exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -14,4 +14,15 @@ const newEmployees = (callback) => {
 };
 
 // console.log(employeeGenerator('Igor Arecippo'));
-console.log(newEmployees(employeeGenerator));
+// console.log(newEmployees(employeeGenerator));
+
+//2
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
