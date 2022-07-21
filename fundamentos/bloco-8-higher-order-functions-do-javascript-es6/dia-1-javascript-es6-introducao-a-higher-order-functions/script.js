dd2470c2b44f4fbeb2e59f6769fdf4acdba4c10f
Igor Arecippo <iarecippo@gmail.com>
2022-07-21 18:56:38 -0300

    exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,17 @@
+//1
+const employeeGenerator = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return {fullName, email: `${email}@trybe.com`};
+};
+
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'),
+      id2: callback('Luiza Drummond'),
+      id3: callback('Carla Paiva'),
+    }
+    return employees;
+};
+
+// console.log(employeeGenerator('Igor Arecippo'));
+console.log(newEmployees(employeeGenerator));
