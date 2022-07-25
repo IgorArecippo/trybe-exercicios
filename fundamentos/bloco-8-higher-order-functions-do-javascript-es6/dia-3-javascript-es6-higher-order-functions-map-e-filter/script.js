e08520ebe43fdcc4334d6eb968b65a9d646efa0c
Igor Arecippo <iarecippo@gmail.com>
2022-07-25 19:24:03 -0300

    exercicio 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -80,4 +80,11 @@ const fantasyOrScienceFiction = (books) => books.filter((book) => book.genre ===
 
 //4
 const oldBooksOrdered = (books) => books.filter((book) => 2022 - book.releaseYear >= 60).sort((book1, book2) => book1.releaseYear - book2.releaseYear);
-console.log(oldBooksOrdered(books));
+// console.log(oldBooksOrdered(books));
+
+//5
+const fantasyOrScienceFictionAuthors = (books) => {
+    const filteredBooks = books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+    return filteredBooks.map((book) => book.author.name).sort();
+};
+console.log(fantasyOrScienceFictionAuthors(books));
