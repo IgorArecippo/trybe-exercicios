fa4a2cf3ba0e4638aee685a43f7341fc2da635b8
Igor Arecippo <iarecippo@gmail.com>
2022-07-26 18:32:09 -0300

    exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script.js
@@ -0,0 +1,11 @@
+//1
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+  
+function flatten() {
+    return arrays.reduce((acc, curr) => acc.concat(curr), []);
+}
+// console.log(flatten());
