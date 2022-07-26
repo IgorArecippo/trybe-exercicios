9b10b55bd0e44e6ba86dc986fc898df23c878f97
Igor Arecippo <iarecippo@gmail.com>
2022-07-26 19:33:46 -0300

    exercicio 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script.js
@@ -75,21 +75,31 @@ const books = [
 
 //2
 const reduceNames = (array) =>  {
-   const names = books.reduce((acc, book, index, array) => {
+   const names = array.reduce((acc, book, index, array) => {
     if (index === array.length -1) return `${acc} ${book.author.name}.`;
     return `${acc} ${book.author.name},`
    }, '');
    return names.trim()
 };
-// console.log(reduceNames(books));
+// console.log(reduceNames array));
 //pq precisou colocar o `${acc}`
 
 //3
-function averageAge() {
-    const numberOfAuthors = books.length;
-    const sumOfAges = books.reduce((sum, age) => (
+function averageAge(array) {
+    const numberOfAuthors = array.length;
+    const sumOfAges = array.reduce((sum, age) => (
         sum + (age.releaseYear - age.author.birthYear)
     ), 0);
     return sumOfAges / numberOfAuthors;
 };
-console.log(averageAge())
+// console.log(averageAge(books))
+// pq precisou colocar () ao inves de {} na declaracao da arrow function do reduce linha 90
+
+//4
+function longestNamedBook(array) {
+    return array.reduce((biggestBook, currentBook) => {
+        if (biggestBook.name.length > currentBook.name.length) return biggestBook;
+        return currentBook;
+    });
+};
+console.log(longestNamedBook(books));
